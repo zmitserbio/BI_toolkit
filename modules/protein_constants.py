b98fe39efa96d281f6702b0b30b73d1259a72ac0
zmitserbio <144448919+zmitserbio@users.noreply.github.com>
2023-10-11 20:53:51 +0300

    Add constants to protein_constants.py

--- modules/protein_constants.py
+++ modules/protein_constants.py
@@ -1,1 +1,34 @@
+retranslation_dict = {
+        'F': 'TTC', 'f': 'ttc',
+        'L': 'TTA', 'l': 'tta',
+        'S': 'TCG', 's': 'tcg',
+        'Y': 'TAC', 'y': 'tac',
+        'C': 'TGC', 'c': 'tgc',
+        'W': 'TGG', 'w': 'tgg',
+        'P': 'CCC', 'p': 'ccc',
+        'H': 'CAT', 'h': 'cat',
+        'Q': 'GAA', 'q': 'gaa',
+        'R': 'CGA', 'r': 'cga',
+        'I': 'ATT', 'i': 'att',
+        'M': 'ATG', 'm': 'atg',
+        'T': 'ACC', 't': 'acc',
+        'N': 'AAT', 'n': 'aat',
+        'K': 'AAA', 'k': 'aaa',
+        'V': 'GTT', 'v': 'gtt',
+        'A': 'GCA', 'a': 'gca',
+        'D': 'GAT', 'd': 'gca',
+        'E': 'GAG', 'e': 'gag',
+        'G': 'GGG', 'g': 'ggg'
+    }
 
+threel = {'A': 'ALA', 'R': 'ARG', 'N': 'ASN', 'D': "ASP", 'V': 'VAL',
+                 'H': 'HIS', 'G': "GLY", 'Q': "GLN", 'E': 'GLU', 'I': 'ILE',
+                 'L': 'LEU', 'K': 'LYS', 'M': 'MET', 'P': 'PRO', 'S': 'SER',
+                 'Y': 'TYR', 'T': 'THR', 'W': 'TRP', 'F': 'PHE', 'C': 'CYS',
+                 'a': 'ala', 'r': 'arg', 'n': 'asn', 'd': "asp", 'v': 'val',
+                 'h': 'his', 'g': "gly", 'q': "gln", 'e': 'glu', 'i': 'ile',
+                 'l': 'leu', 'k': 'lys', 'm': 'met', 'p': 'pro', 's': 'ser',
+                 'y': 'tyr', 't': 'thr', 'w': 'trp', 'f': 'phe', 'c': 'cys'
+    }
+
+aminoacids = ['F', 'f', 'L', 'l', 'S', 's', 'Y', 'y', 'C', 'c', 'W', 'w', 'P', 'p', 'H', 'h', 'Q', 'q', 'R', 'r', 'I', 'i', 'M', 'm', 'T', 't', 'N', 'n', 'K', 'k', 'V', 'v', 'A', 'a', 'D', 'd', 'E', 'e', 'G', 'g']
