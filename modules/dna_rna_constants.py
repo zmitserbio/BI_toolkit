3ee27057c52f7b9596de2be2f72295511aa315de
zmitserbio <144448919+zmitserbio@users.noreply.github.com>
2023-10-11 20:49:41 +0300

    Add constants to dna_rna_constants.py

--- modules/dna_rna_constants.py
+++ modules/dna_rna_constants.py
@@ -1,1 +1,3 @@
-
+dna_rna_alphabet = ['A', 'a', 'T', 't', 'C', 'c', 'G', 'g', 'U', 'u']
+compl_dna = {'A': 'T', 'a': 't', 'T': 'A', 't': 'a', 'C': 'G', 'c': 'g', 'G': 'C', 'g': 'c'}
+compl_rna = {'A': 'U', 'a': 'u', 'U': 'A', 'u': 'a', 'C': 'G', 'c': 'g', 'G': 'C', 'g': 'c'}
